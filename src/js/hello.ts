cdf87475d9b103902e155d1f31eb01a049f29f50
naoiwata <orrrizzle@gmail.com>
2014-04-28 01:06:54 +0900

    Add html package

---
+++ src/js/hello.ts
@@ -0,0 +1,13 @@
+module Greeting {
+    export class Hello {
+        constructor(private text : string) {
+        }
+        say() : void {
+            console.log(this.text);
+        }
+    }
+}
+
+var hello : Greeting.Hello = new Greeting.Hello("Hello, World!");
+hello.say();
+
